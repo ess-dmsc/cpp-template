9285dc0d61e761545199e0a98cc6f7afbded6044
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2018-10-18 14:53:30 +0100

    Basic method with Doxygen documentation

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
+/// Square a double.
+///
+/// Typical usage:
+/// \code
+///   square(3.8);
+/// \endcode
+///
+/// \param val Double value to be squared.
+///
+/// \returns The square of `val`.
+double square(double val) {
+  return val * val;
+}
+
 int main(int argc, char **argv) {
   std::cout << "Hello World" << std::endl;
 }
